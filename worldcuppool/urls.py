cb43de27d1f7d0956baa5c735e02711a91903cbc
Andrew Miller <amiller@millergis.com>
2014-06-18 09:25:54 -0700

    add picked team display

--- worldcuppool/urls.py
+++ worldcuppool/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from worldcuppool import views
 
 urlpatterns = [
-    url(r'^$', views.get_standings, name='index')
+    url(r'^$', views.get_standings, name='index'),
+    url(r'^user/(?P<userID>[0-9]+)/$',views.get_picks, name='user_picks'),
 ]
--- worldcuppool/views.py
+++ worldcuppool/views.py
@@ -3,6 +3,28 @@ from django.shortcuts import render
 from worldcuppool.models import User, Team
 
 
+def get_picks(request, userID):
+    user = User.objects.get(userID=userID)
+    picks = []
+    picks.append(("16pt",user.pick16.name))
+    picks.append(("15pt",user.pick15.name))
+    picks.append(("14pt",user.pick14.name))
+    picks.append(("13pt",user.pick13.name))
+    picks.append(("12pt",user.pick12.name))
+    picks.append(("11pt",user.pick11.name))
+    picks.append(("10pt",user.pick10.name))
+    picks.append(("9pt",user.pick9.name))
+    picks.append(("8pt",user.pick8.name))
+    picks.append(("7pt",user.pick7.name))
+    picks.append(("6pt",user.pick6.name))
+    picks.append(("5pt",user.pick5.name))
+    picks.append(("4pt",user.pick4.name))
+    picks.append(("3pt",user.pick3.name))
+    picks.append(("2pt",user.pick2.name))
+    picks.append(("1pt",user.pick1.name))
+
+    return render(request, 'picks_table.html', {'picks':picks})
+
 def get_standings(request):
     users = User.objects.all()
     standings_table = {}
@@ -13,7 +35,7 @@ def get_standings(request):
     rank = 0
     for key, value in sorted(points.iteritems(), key=lambda (k,v): (v,k), reverse=True):
         rank += 1
-        standings_table[rank] = (key, value)
+        standings_table[rank] = (key.userID, key, value)
 
     return render(request, 'standings_table.html', {'standings_table':standings_table})
 
